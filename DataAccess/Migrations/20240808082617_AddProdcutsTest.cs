2c969009015d8fa8f400d5fa1101dd0e9a1d5dea
Bilgehan Demirkaya <>
2024-08-08 11:45:42 +0300

    adding root files

---
+++ DataAccess/Migrations/20240808082617_AddProdcutsTest.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProdcutsTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manifactors_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Manifactors");
+
+            migrationBuilder.CreateTable(
+                name: "Manufacturers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manufacturers", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manufacturers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Manufacturers_ManufacturerId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Manufacturers");
+
+            migrationBuilder.CreateTable(
+                name: "Manifactors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Manifactors", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Manifactors_ManufacturerId",
+                table: "Products",
+                column: "ManufacturerId",
+                principalTable: "Manifactors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
